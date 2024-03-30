138254411284bbe6a0ed587573710a16e98038f7
kuba <j.mieczkowski00@gmail.com>
2024-03-30 02:03:19 +0100

    deleting unnecessary imports and add validAtrrName method

--- src/main/java/com/example/systemconfigurationparametersmanagerapi/endpoints/SVConfEndpoint.java
+++ src/main/java/com/example/systemconfigurationparametersmanagerapi/endpoints/SVConfEndpoint.java
@@ -1,14 +1,19 @@
 package com.example.systemconfigurationparametersmanagerapi.endpoints;
 
+import com.example.systemconfigurationparametersmanagerapi.exceptions.BlankAttributeException;
+import com.example.systemconfigurationparametersmanagerapi.exceptions.ObjectNotValidException;
 import com.example.systemconfigurationparametersmanagerapi.model.SvConf;
 import com.example.systemconfigurationparametersmanagerapi.services.SvConfService;
 import com.example.systemconfigurationparametersmanagerapi.svconf.*;
 import com.example.systemconfigurationparametersmanagerapi.validator.ObjectValidator;
+import jakarta.validation.Path;
 import org.springframework.beans.BeanUtils;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import java.util.Map;
 
 @Endpoint
 public class SVConfEndpoint {
@@ -39,6 +44,8 @@ public class SVConfEndpoint {
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getSvConfRequest")
     @ResponsePayload
     public GetSvConfResponse getSvConf(@RequestPayload GetSvConfRequest request) {
+        validAtrrName(request.getAttrName());
+
         SvConfInfo svConfInfo = new SvConfInfo();
         BeanUtils.copyProperties(svConfService.getSvConfByAttrName(request.getAttrName()), svConfInfo);
 
@@ -64,6 +71,8 @@ public class SVConfEndpoint {
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "deleteSvConfRequest")
     @ResponsePayload
     public DeleteSvConfResponse deleteSvConf(@RequestPayload DeleteSvConfRequest request) {
+        validAtrrName(request.getAttrName());
+
         svConfService.deleteSvConfByAttrName(request.getAttrName());
 
         ServiceStatus serviceStatus = createServiceStatus("SUCCESS", "SvConf has been deleted.");
@@ -77,5 +86,11 @@ public class SVConfEndpoint {
         serviceStatus.setStatus(status);
         serviceStatus.setMessage(message);
         return serviceStatus;
+    }
+
+    private void validAtrrName(String attrName) {
+        if (attrName == null || attrName.isEmpty()) {
+            throw new BlankAttributeException("The attrName cannot be null or empty.");
+        }
     }
 }
